50b7b439605134263af03fc2df2e2de3b45e7e85
Jerry(이재헌) <ti6419@gmail.com>
2022-06-30 16:02:17 +0900

    test 추가 (9개)

--- src/test/java/com/example/tess4j/JUnitTest.java
+++ src/test/java/com/example/tess4j/JUnitTest.java
@@ -21,6 +21,46 @@ public class JUnitTest {
         run("images/hi.png");
     }
 
+    @Test
+    public void test2() throws TesseractException {
+        run("images/hiworks.png");
+    }
+
+    @Test
+    public void test3() throws TesseractException {
+        run("images/ocrtesttext.png");
+    }
+
+    @Test
+    public void test4() throws TesseractException {
+        run("images/wheel.bmp");
+    }
+
+    @Test
+    public void test5() throws TesseractException {
+        run("images/편지쓰기.png");
+    }
+
+    @Test
+    public void test6() throws TesseractException {
+        run("images/confluence.png");
+    }
+
+    @Test
+    public void test7() throws TesseractException {
+        run("images/발송실패.png");
+    }
+
+    @Test
+    public void test8() throws TesseractException {
+        run("images/부정로그인.png");
+    }
+
+    @Test
+    public void test9() throws TesseractException {
+        run("images/kbs.png");
+    }
+
     private void run(String path) throws TesseractException {
         File image = new File(path);
         long beforeTime = System.currentTimeMillis();
@@ -30,7 +70,9 @@ public class JUnitTest {
         long afterTime = System.currentTimeMillis();
         long performanceTime = (afterTime - beforeTime);
 
-        System.out.println("결과 = " + s);
+        System.out.println("결과 = ");
+        System.out.println(s);
+        System.out.println("파일 크기 = " + image.length() / 1024 + "kb");
         System.out.println("수행 시간 = " + performanceTime + "ms");
     }
 
