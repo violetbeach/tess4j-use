72d534092e09de835932ce3ec334949f0a446337
Jerry(이재헌) <ti6419@gmail.com>
2022-06-30 15:29:34 +0900

    performance test 추가

--- src/test/java/com/example/tess4j/JUnitTest.java
+++ src/test/java/com/example/tess4j/JUnitTest.java
@@ -1,19 +1,37 @@
 package com.example.tess4j;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
 public class JUnitTest {
 
-    @Test
-    public void test1() throws TesseractException {
-        Tesseract tesseract = new Tesseract();
-        File image = new File("images/hi.png");
+    static Tesseract tesseract = new Tesseract();
+
+    @BeforeAll
+    static void setup() {
         tesseract.setDatapath("/opt/homebrew/Cellar/tesseract/5.1.0/share/tessdata");
         tesseract.setLanguage("eng+kor");
-        String s = tesseract.doOCR(image);
-        System.out.println(s);
     }
+
+    @Test
+    public void test1() throws TesseractException {
+        run("images/hi.png");
+    }
+
+    private void run(String path) throws TesseractException {
+        File image = new File(path);
+        long beforeTime = System.currentTimeMillis();
+
+        String s = tesseract.doOCR(image);
+
+        long afterTime = System.currentTimeMillis();
+        long performanceTime = (afterTime - beforeTime);
+
+        System.out.println("결과 = " + s);
+        System.out.println("수행 시간 = " + performanceTime + "ms");
+    }
+
 }
